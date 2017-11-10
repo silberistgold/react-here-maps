a12d204702f0bdb0397eae6c9a7d34bca6226241
Paul Heidicker <paulheidicker@gmail.com>
2017-11-10 18:00:10 +0100

    adds start and end properties, adds marker rendering and route rendering, adds property to hide map controls

--- src/HEREMap.tsx
+++ src/HEREMap.tsx
@@ -7,6 +7,13 @@ import cache, { onAllLoad } from "./utils/cache";
 import getLink from "./utils/get-link";
 import getPlatform from "./utils/get-platform";
 import getScriptMap from "./utils/get-script-map";
+import {boxIconSVG, truckIconSVG} from './utils/icons'
+
+// declare an interface for a simple location object
+export interface HERELocation {
+  lat: number;
+  lon: number;
+}
 
 // declare an interface containing the required and potential
 // props that can be passed to the HEREMap component
@@ -17,7 +24,10 @@ export interface HEREMapProps extends H.Map.Options {
   animateZoom?: boolean;
   hidpi?: boolean;
   interactive?: boolean;
+  controlsVisible?: boolean;
   secure?: boolean;
+  start?: HERELocation;
+  end?: HERELocation;
 }
 
 // declare an interface containing the potential state flags
@@ -25,6 +35,10 @@ export interface HEREMapState {
   map?: H.Map;
   behavior?: H.mapevents.Behavior;
   ui?: H.ui.UI;
+  router?: H.service.RoutingService;
+  startMarker?: H.map.Marker;
+  endMarker?: H.map.Marker;
+  route?: any;
 }
 
 // declare an interface containing the context to be passed through the heirarchy
@@ -57,6 +71,16 @@ export class HEREMap
 
     // bind all event handling methods to this
     this.resizeMap = this.resizeMap.bind(this);
+    this.addRoute = this.addRoute.bind(this);
+    this.onRouteSuccess = this.onRouteSuccess.bind(this);
+    this.onRouteError = this.onRouteError.bind(this);
+    this.addRouteShapeToMap = this.addRouteShapeToMap.bind(this);
+    this.removeRouteShapeFromMap = this.removeRouteShapeFromMap.bind(this);
+    this.addMarkerToMap = this.addMarkerToMap.bind(this);
+    this.setStartIcon = this.setStartIcon.bind(this);
+    this.setEndIcon = this.setEndIcon.bind(this);
+    this.removeStartIcon = this.removeStartIcon.bind(this);
+    this.removeEndIcon = this.removeEndIcon.bind(this);
 
     // debounce the resize map method
     this.debouncedResizeMap = debounce(this.resizeMap, 200);
@@ -75,8 +99,11 @@ export class HEREMap
         center,
         hidpi,
         interactive,
+        controlsVisible,
         secure,
         zoom,
+        start,
+        end,
       } = this.props;
 
       // get the platform to base the maps on
@@ -99,8 +126,37 @@ export class HEREMap
           center,
           pixelRatio: hidpi ? 2 : 1,
           zoom,
+          imprint: null
         },
       );
+
+      const mapTileService = platform.getMapTileService({ 'type': 'base' });
+      const fleetStyleLayer = mapTileService.createTileLayer(
+        'maptile',
+        'normal.day',
+        256,
+        'png8',
+        { 'style': 'flame' }
+      );
+
+      map.setBaseLayer(fleetStyleLayer);
+
+      // configure router
+      const router = platform.getRoutingService();
+      this.setState({
+        router
+      });
+
+      console.log(this.props);
+
+      // add markers and route if already given
+      if (start) {
+        console.log('TRYING TO SET START')
+        this.setStartIcon(map, start, false);
+      }
+      if (end) {
+        this.setEndIcon(map, end, false);
+      }
 
       if (interactive !== false) {
         // make the map interactive
@@ -111,12 +167,17 @@ export class HEREMap
         // create the default UI for the map
         const ui = H.ui.UI.createDefault(map, defaultLayers);
 
+        if (controlsVisible === false) {
+          ui.getControl('mapsettings').setVisibility(false)
+          ui.getControl('scalebar').setVisibility(false)
+          ui.getControl('zoom').setVisibility(false)
+        }
+
         this.setState({
           behavior,
           ui,
         });
       }
-
       // make the map resize when the window gets resized
       window.addEventListener("resize", this.debouncedResizeMap);
 
@@ -138,6 +199,79 @@ export class HEREMap
   public componentWillUnmount() {
     // make the map resize when the window gets resized
     window.removeEventListener("resize", this.debouncedResizeMap);
+  }
+
+  public componentWillUpdate(nextProps: HEREMapProps, nextState: HEREMapState, nextContext: object) {
+
+    if (this.props.start !== nextProps.start) {
+      // something has changed with the start position
+      const {map} = this.state;
+      if (map) {
+        if (!this.props.start && nextProps.start) {
+          this.setStartIcon(map, nextProps.start, true);
+        } else if (this.props.start && !nextProps.start) {
+          this.removeStartIcon(map);
+        } else if (this.props.start && nextProps.start && this.props.start !== nextProps.start) {
+          this.removeStartIcon(map);
+          this.setStartIcon(map, nextProps.start, true);
+        }
+      }
+    }
+
+    if (this.props.end !== nextProps.end) {
+      const {map} = this.state;
+      if (map) {
+        if (!this.props.end && nextProps.end) {
+            this.setEndIcon(map, nextProps.end, false);
+        } else if (this.props.end && !nextProps.end) {
+          this.removeEndIcon(map);
+        } else if (this.props.end && nextProps.end && this.props.end !== nextProps.end) {
+          this.removeEndIcon(map);
+          this.setEndIcon(map, nextProps.end, false);
+        }
+      }
+    }
+
+    if (nextProps.start && nextProps.end) {
+      // we can draw a route, check if we should update
+      if ((this.props.start !== nextProps.start) || (this.props.end !== nextProps.end)) {
+        this.removeRouteShapeFromMap()
+        this.addRoute(nextProps.start, nextProps.end);
+      }
+    } else {
+      // nor oute to draw, remove if possibe
+      this.removeRouteShapeFromMap()
+    }
+  }
+
+  private setStartIcon(map: H.Map, location: HERELocation, shouldZoomIntoView: boolean) {
+    const size = new H.math.Size(144, 100);
+    const truckIcon = new H.map.Icon(truckIconSVG(), {size: size, crossOrigin: false});
+    const startMarker = this.addMarkerToMap(map, location, truckIcon, shouldZoomIntoView);
+    this.setState({startMarker: startMarker});
+  }
+
+  private removeStartIcon(map: H.Map) {
+    const {startMarker} = this.state;
+    if (startMarker) {
+      map.removeObject(startMarker);
+    }
+    this.setState({startMarker: null});
+  }
+
+  private setEndIcon(map: H.Map, location: HERELocation, shouldZoomIntoView: boolean) {
+    const size = new H.math.Size(115, 80);
+    const boxIcon = new H.map.Icon(boxIconSVG(), {size: size, crossOrigin: false});
+    const endMarker = this.addMarkerToMap(map, location, boxIcon, false)
+    this.setState({endMarker: endMarker});
+  }
+
+  private removeEndIcon(map: H.Map) {
+    const {endMarker} = this.state;
+    if (endMarker) {
+      map.removeObject(endMarker);
+    }
+    this.setState({endMarker: null});
   }
 
   public render() {
@@ -164,6 +298,96 @@ export class HEREMap
     if (map) {
       map.getViewPort().resize();
     }
+  }
+
+  private removeRouteShapeFromMap() {
+    const { map, route } = this.state;
+
+    if (map && route) {
+      map.removeObject(route);
+      this.setState({
+        route: null
+      });
+    }
+  }
+
+  private addRoute(start: HERELocation, end: HERELocation) {
+    const { router } = this.state;
+
+    if (router) {
+      const routeRequestParams = {
+        mode: 'fastest;car',
+        representation: 'display',
+        routeattributes : 'waypoints,summary,shape,legs',
+        maneuverattributes: 'direction,action',
+        waypoint0: `${start.lat},${start.lon}`,
+        waypoint1: `${end.lat},${end.lon}`
+      };
+
+      router.calculateRoute(
+        routeRequestParams,
+        this.onRouteSuccess,
+        this.onRouteError
+      );
+    }
+  }
+
+  private onRouteSuccess(result: any) {
+    const route = result.response.route[0];
+    this.addRouteShapeToMap(route);
+  }
+
+  private onRouteError(error: Error) {
+    console.error(error);
+    alert(error);
+  }
+
+  private addRouteShapeToMap(route: any) {
+    const lineString = new H.geo.LineString();
+    const routeShape = route.shape;
+
+    routeShape.forEach(function(point: any) {
+      const parts = point.split(',');
+      lineString.pushLatLngAlt(parts[0], parts[1]);
+    });
+
+    const polyline = new H.map.Polyline(lineString, {
+      style: {
+        lineWidth: 4,
+        strokeColor: '#fa8872'
+      }
+    });
+
+    this.setState({
+      route: polyline
+    });
+
+    const { map } = this.state;
+
+    if (map) {
+      // Add the polyline to the map
+      map.addObject(polyline);
+      // And zoom to its bounding rectangle
+      const lineBounds = polyline.getBounds();
+      const factor = 0.5;
+      const widthDeltaPerSide = lineBounds.getWidth() * factor / 2;
+      const heightDeltaPerSide = lineBounds.getHeight() * factor / 2;
+      const newBounds = new H.geo.Rect(lineBounds.getTop() - heightDeltaPerSide, lineBounds.getLeft() - widthDeltaPerSide, lineBounds.getBottom() + heightDeltaPerSide, lineBounds.getRight() + widthDeltaPerSide);
+      map.setViewBounds(newBounds, true);
+    }
+  }
+
+  private addMarkerToMap(map: H.Map, location: HERELocation, icon: H.map.Icon, shouldZoomToMarker: boolean): H.map.Marker {
+
+    const hereLocation = {lat: location.lat, lng: location.lon};
+    const marker = new H.map.Marker(hereLocation, {icon: icon});
+
+      map.addObject(marker);
+      if (shouldZoomToMarker) {
+        map.setCenter(hereLocation, true);
+        map.setZoom(17, true);
+      }
+      return marker
   }
 }
 
